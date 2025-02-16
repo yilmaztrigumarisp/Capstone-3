4342fe0ab24319033ddf7026f6971e6a69f3e22c
yilmaztrigumarisp <yilmaztrigumari28@gmail.com>
2025-02-17 01:36:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import numpy as np
 import pandas as pd
 
 # Load the trained CatBoost model
-model = joblib.load("final_catboost_model.pkl")
-
+# model = joblib.load("final_catboost_model.pkl")
+with open(r'final_catboost_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+    
 # App Title
 st.title("🏡 California House Price Prediction")
 
